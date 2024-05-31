85a3fc4d94922ae54fbf96ddc2211b0a52209922
sudhamadhavi1 <madhavissvs3@gmail.com>
2024-05-31 19:18:47 +0530

    updated like button & count

---
+++ src/components/EachMessage/EachMessage.jsx
@@ -0,0 +1,40 @@
+import './index.css';
+import { AiOutlineLike } from 'react-icons/ai';
+import { useState } from 'react';
+
+const EachMessage = (props) => {
+  const { eachMessage } = props;
+  const [count, setCount] = useState(0);
+
+  const onClickLike = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <li key={eachMessage.id} className='each-message-conatiner'>
+      <div
+        className='user-profile'
+        style={{
+          backgroundColor: `${eachMessage.messagefrom.backgroundColor}`,
+        }}
+      >
+        <p className='letter'>{eachMessage.profileInitial}</p>
+      </div>
+      <div className='right-section'>
+        <div className='top-section'>
+          <h1 className='user-name'>{eachMessage.messagefrom.user}</h1>
+          <span className='time'>{eachMessage.messageSentTime}</span>
+        </div>
+        <div className='each-message'>
+          {eachMessage.message}
+          <button type='button' onClick={onClickLike} className='like-button'>
+            <AiOutlineLike className='like-icon' />
+          </button>
+          {count > 0 ? <p>{count}</p> : null}
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EachMessage;
--- src/components/MessageDelivered/MessageDelivered.jsx
+++ src/components/MessageDelivered/MessageDelivered.jsx
@@ -1,9 +1,6 @@
 import './index.css';
-//  id: uuid(),
-//           user: messagefrom,
-//           message: messageEntered,
-//           profileInitial: messagefrom[0],
-//           messageSentTime: time,
+import { AiOutlineLike } from 'react-icons/ai';
+import EachMessage from '../EachMessage/EachMessage';
 
 const MessageDelivered = (props) => {
   const { messageDetails } = props;
@@ -12,23 +9,7 @@ const MessageDelivered = (props) => {
     <div className='message-delivered-container'>
       <ul className='all-messages-conatiner'>
         {messageDetails.map((eachMessage) => (
-          <li key={eachMessage.id} className='each-message-conatiner'>
-            <div
-              className='user-profile'
-              style={{
-                backgroundColor: `${eachMessage.messagefrom.backgroundColor}`,
-              }}
-            >
-              <p className='letter'>{eachMessage.profileInitial}</p>
-            </div>
-            <div className='right-section'>
-              <div className='top-section'>
-                <h1 className='user-name'>{eachMessage.messagefrom.user}</h1>
-                <span className='time'>{eachMessage.messageSentTime}</span>
-              </div>
-              <div className='each-message'>{eachMessage.message}</div>
-            </div>
-          </li>
+          <EachMessage eachMessage={eachMessage} />
         ))}
       </ul>
     </div>
